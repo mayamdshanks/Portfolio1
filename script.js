7cced809376435630c09f35c3e1cc487ab43b962
Umanga Pokharel <umanga501@gmail.comm>
2023-05-18 22:30:18 +0545

    PortfolioVersion1

---
+++ script.js
@@ -0,0 +1,67 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+  
+};
+// document.addEventListener("click", (event) => {
+//   const isCertainSectionClicked = event.target.closest("#menu-icon");
+  
+//   if (!isCertainSectionClicked) {
+//     menuIcon.classList.add("bx-x");
+//     navbar.classList.add("active");
+//   }
+// });
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href *=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+
+// new line
+// let header = document.querySelector("header");
+// header.classList.toggle("sticky".window.scrollY > 100);
+// menuIcon.classList.remove("bx-x");
+// navbar.classList.remove("active");
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin:'top', delay:100});
+ScrollReveal().reveal('.social-media, .about-content ,.about-image', { delay: 200 });
+
+const typed= new Typed('.typed-text',{
+  strings:['Web developer','designer'],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
+
+// const myName={
+//   name:'umanga',
+//   class:'bachelors'
+// }
+
+// console.log(myName.name)
